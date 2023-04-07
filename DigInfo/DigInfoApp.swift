b242b5fc7fa4f63d8aefd12875f6fe2671e8aa3e
dhimaspp <dhimas.putera.pangestu@bni.co.id>
2023-04-07 10:21:58 +0700

    Initial Commit

---
+++ DigInfo/DigInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigInfoApp.swift
+//  DigInfo
+//
+//  Created by Dhimas P Pangestu on 07/04/23.
+//
+
+import SwiftUI
+
+@main
+struct DigInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
